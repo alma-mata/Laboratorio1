a20880e7108deb54c03dc993b4c597690aedd950
alma-mata <mat23038@uvg.edu.gt>
2025-02-12 00:15:37 -0600

    POST LAB
    Se hacen muchos cambios para poder cumplir con el post laboratorio, los cambios son:
    1. Se cambia la salida de ambos contadores al PORT D
    2. Se agrega una función para combinar ambos contadores y mostrarlos en una salida
    3. PORT B se usa para mostrar exclusivamente el sumador
    4. Se hace la función de suma y se cambia la frecuencia del oscilador.
    5. Se eliminan las subrutinas de overflow y underflow, pues fueron reemplazadas por ANDI.
    Creo que son todos los cambios.

--- Laboratorio1/Laboratorio1/main.asm
+++ Laboratorio1/Laboratorio1/main.asm
@@ -20,12 +20,19 @@ OUT SPH, R16
 
 // Configuración MCU
 SETUP:
+	// Configuración del CLOCK en 1 MHz
+	LDI		R16, 0x80		// El valor habilita cambios en CLKPR
+	STS		CLKPR, R16		// CLKPR divide el oscilador
+	LDI		R16, 0x03		// 3 es equivalente a 8
+	STS		CLKPR, R16		// Se carga el valor para 8MHz/8 = 1MHz
+
 	// Configurar pines de entrada y salida (DDRx, PORTx, PINx)
 	// PORTC como entrada con pull-up habilitado
 	LDI		R16, 0x00
 	OUT		DDRC, R16	// Establecer puerto C como entrada
 	LDI		R16, 0xFF
 	OUT		PORTC, R16	// Habilitar pull-ups en puerto C
+
 // Modificar PORT D como unica salida de contadores y PORT B como salida de sumador.
 	// PORTB como salida inicialmente apagado
 	LDI		R16, 0xFF
@@ -42,6 +49,8 @@ SETUP:
 	LDI		R17, 0xFF	// Variable que guarda el estado de los botones
 	LDI		R18, 0x00	// Variable para llevar el contador 1
 	LDI		R20, 0x00	// Variable para llevar el contador 2
+	LDI		R21, 0x00	// Guarda combinacion de R18 y R20
+	LDI		R22, 0x00	// Variable para la sumatoria
 
 // Loop infinito
 MAIN:
@@ -59,54 +68,44 @@ MAIN:
 	// Lógica para aumento o decremento del contador
 	CALL	CONTADOR_1
 	CALL	CONTADOR_2
-	// Agregar subrutina para combinar ambos contadores en una salida
-	// Agregar sumador
-	RJMP	MAIN
+	CALL	COMBINACION		// Subrutina para combinar ambos contadores en una salida
 
+	// Sumador de contadores
+	SBRS	R16, 0		// Salta si Bit 0 de PORTC esta en 1 (apagado)
+	CALL	SUMADOR		// Llama al sumador
+
+	RJMP	MAIN
 
 CONTADOR_1:
 	SBRS	R16, 2			// Salta si Bit 2 de PORTC esta en 1 (apagado)
 	INC		R18				// Incrementa en 1 al contador
-	CPI		R18, 0x10		// Comparación para identificar Overflow
-	BREQ	OVERFLOW_1		// Salta al bloque del overflow
 	SBRS	R16, 3			// Salta si Bit 3 de PORTC esta en 1 (apagado)
 	DEC		R18				// Resta 1 al contador
-	CPI		R18, 0xFF		// Comparación para identificar Underflow
-	BREQ	UNDERFLOW_1		// Salta al bloque del underflow
-	OUT		PORTB, R18		// Salida del contador
-	RET						// Regresa al llamado de CONTADOR_1
-
-OVERFLOW_1:
-	LDI		R18, 0x00		// Asigna valor 0 al contador
-	OUT		PORTB, R18		// Muestra valor en la salida
-	RET						// Regresa al llamado de CONTADOR_1
-
-UNDERFLOW_1:
-	LDI		R18, 0x0F		// Asigna valor 15 al contador
-	OUT		PORTB, R18		// Muestra valor en la salida
-	RET						// Regresa al llamado de CONTADOR_1
+	ANDI	R18, 0x0F		// Mascara para evitar Overflow
+	RET						// Regresa al CALL
 
 CONTADOR_2:					//Misma lógica que CONTADOR_1
 	SBRS	R16, 4			// Salta si Bit 4 de PORTC esta en 1 (apagado)
 	INC		R20				// Salta al bloque aumento
-	CPI		R20, 0x10		
-	BREQ	OVERFLOW_2
 	SBRS	R16, 5			// Salta si Bit 5 de PORTC esta en 1 (apagado)
-	DEC		R20				// Salta al bloque decremento
-	CPI		R20, 0xFF
-	BREQ	UNDERFLOW_2
-	OUT		PORTD, R20
-	RET						// Regresa al inicio
+	DEC		R20				// Resta 1 al contador
+	ANDI	R20, 0x0F		// Mascara para evitar Overflow
+	RET						// Regresa al CALL
 
-OVERFLOW_2:					// Hace lo mismo que OVERFLOW_1
-	LDI		R20, 0x00
-	OUT		PORTB, R18
-	RET
+COMBINACION:	// Sub rutina para combinar contadores
+	SWAP	R20				// Intercambia los 4 bits más y menos significativos
+	MOV		R21, R18		// Guarda R18 en R21
+	OR		R21, R20		// Combina ambos contadores con la operación OR
+	SWAP	R20				// Regresa R20 a su estado original
+	OUT		PORTD, R21		// Muestra R21 en PORT D
+	RET						// Regresa a donde fue llamado
 
-UNDERFLOW_2:				// Hace lo mismo que UNDERFLOW_1
-	LDI		R20, 0x0F
-	OUT		PORTB, R18
-	RET
+SUMADOR:			// Sumatoria de ambos contadores
+	MOV		R22, R18		// Copia R18 en R22
+	ADD		R22, R20		// Suma Contador 1 y 2
+	ANDI	R22, 0x1F		// Mascara para evitar Overflow
+	OUT		PORTB, R22		// Muestra la salida
+	RET						// Regresa al CALL
 
 // Rutina de interrupción
 DELAY:
